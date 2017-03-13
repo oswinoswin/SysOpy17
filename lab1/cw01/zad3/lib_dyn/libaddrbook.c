ec46c66754a1f5110a47732486166de18ff2c1b4
oswinoswin <oswinoswin@github.com>
2017-03-13 18:12:29 +0100

    zad4 complete

---
+++ lab1/cw01/zad3/lib_dyn/libaddrbook.c
@@ -0,0 +1,526 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "libaddrbook.h"
+
+LIST *createList() {
+    LIST *tmp = malloc(sizeof(LIST));
+    if(tmp == NULL) {
+        fprintf(stderr, "Malloc failed!\n");
+        exit(EXIT_FAILURE);
+    }
+    tmp->head = NULL;
+    tmp->tail = NULL;
+    return tmp;
+}
+
+NODE *createNode(char *firstName, char *lastName, char *birthDate,
+                 char *email, char *phone) {
+    NODE *tmp = malloc(sizeof(NODE));
+    if(tmp == NULL) {
+        fprintf(stderr, "Malloc failed!\n");
+        exit(EXIT_FAILURE);
+    }
+
+    memset(tmp->firstName, '\0', sizeof(tmp->firstName)); //allpcate memory
+    strncpy(tmp->firstName, firstName, BUFFER_LEN); //save values
+
+    memset(tmp->lastName, '\0', sizeof(tmp->lastName));
+    strncpy(tmp->lastName, lastName, BUFFER_LEN);
+
+    memset(tmp->birthDate, '\0', sizeof(tmp->birthDate));
+    strncpy(tmp->birthDate, birthDate, BUFFER_LEN);
+
+    memset(tmp->email, '\0', sizeof(tmp->email));
+    strncpy(tmp->email, email, BUFFER_LEN);
+
+    memset(tmp->phone, '\0', sizeof(tmp->phone));
+    strncpy(tmp->phone, phone, BUFFER_LEN);
+
+    tmp->next = NULL;
+    tmp->prev = NULL;
+
+    return tmp;
+}
+
+void lAddFront(NODE *node, LIST *list) {
+    if(node != NULL && list != NULL) {
+        if(list->head != NULL) {
+            node->next = list->head;
+            list->head->prev = node;
+            list->head = node;
+        } else {
+            list->head = node;
+            list->tail = node;
+        }
+    }
+}
+
+void lAddBack(NODE *node, LIST *list) {
+    if(node != NULL && list != NULL) {
+        if(list->head != NULL) {
+            node->prev = list->tail;
+            list->tail->next = node;
+            list->tail = node;
+        } else {
+            list->head = node;
+            list->tail = node;
+        }
+    }
+}
+
+void lDeleteNode(NODE *node, LIST *list) {
+    if(node != NULL && list != NULL) {
+        if(lIsInList(node, list) == 1) {
+            if(list->head == node) {
+                list->head = node->next;
+                node->next->prev = NULL;
+            } else if(list->tail == node) {
+                list->tail = node->prev;
+                node->prev->next = NULL;
+            } else {
+                node->next->prev = node->prev;
+                node->prev->next = node->next;
+            }
+        }
+    }
+    free(node);
+}
+
+void lDeleteList(LIST *list) {
+    NODE *tmp, *point;
+    if(list != NULL) {
+        tmp = list->head;
+        while(tmp != NULL) {
+            point = tmp;
+            tmp = tmp->next;
+            free(point);
+        }
+        free(list);
+    }
+}
+
+NODE *lFindByIndex( int index, LIST *list) {
+    NODE *tmp;
+    if(list != NULL) {
+        tmp = list->head;
+        for(int i = 1; i < index && tmp != NULL; i++) {
+            tmp = tmp->next;
+        }
+    }
+    return tmp;
+}
+
+NODE *lFindPerson( char *firstName, char *lastName, LIST *list) {
+    NODE *tmp = NULL;
+    if(list != NULL) {
+        tmp = list->head;
+        while(tmp != NULL && (strcmp(tmp->firstName, firstName) != 0)
+              && (strcmp(tmp->lastName, lastName)) != 0) {
+            tmp = tmp->next;
+        }
+    }
+    return tmp;
+}
+
+int lIsInList(NODE *node, LIST *list) {
+    if(list != NULL && node != NULL) {
+        NODE *tmp = NULL;
+        if(list->head != NULL) {
+            tmp = list->head;
+            while(tmp != NULL && tmp != node) {
+                tmp = tmp->next;
+            }
+            return (tmp != NULL) ? 1 : 0;
+        }
+    }
+    return 0;
+}
+
+//insert-sort
+void lSort(LIST *list, char *key) {
+    if(list != NULL) {
+        NODE *tmpHead = list->head;
+        NODE *tmpActual, *tmp;
+
+        while(tmpHead != NULL) {
+            tmpActual = tmpHead->next;
+            while(tmpActual != NULL && lCompare2(tmpHead, tmpActual, key) <= 0) {
+                tmpActual = tmpActual->next;
+            }
+            if(tmpActual != NULL) {
+                tmp = tmpHead->next;
+                lSwap(tmpHead, tmpActual, list);
+                tmpHead = tmp;
+            } else {
+                tmpHead = tmpHead->next;
+            }
+        }
+    }
+}
+
+void lSwap(NODE *node1, NODE *node2, LIST *list) {
+    if((node1 != NULL && node2 != NULL) && (node1 != node2)) {
+        NODE *tmp1, *tmp2;
+        if(list->head == node1) {
+            list->head = node2;
+        } else if(list->head == node2) {
+            list->head = node1;
+        }
+
+        if(list->tail == node1) {
+            list->tail = node2;
+        } else if(list->tail == node2) {
+            list->tail = node1;
+        }
+
+        tmp1 = node1->next;
+        tmp2 = node2->next;
+        node1->next = tmp2;
+        node2->next = tmp1;
+        if(tmp1 != NULL) tmp1->prev = node2;
+        if(tmp2 != NULL) tmp2->prev = node1;
+
+        tmp1 = node1->prev;
+        tmp2 = node2->prev;
+        node1->prev = tmp2;
+        node2->prev = tmp1;
+        if(tmp1 != NULL) tmp1->next = node2;
+        if(tmp2 != NULL) tmp2->next = node1;
+    }
+}
+
+int lCompare(NODE *node1, NODE *node2) {
+    if(node1 != NULL && node2 != NULL) {
+        int cmpResult = strcmp(node1->firstName, node2->firstName);
+        if(cmpResult < 0) {
+            return -1;
+        } else if(cmpResult > 0) {
+            return 1;
+        } else {
+            return strcmp(node1->lastName, node2->lastName);
+        }
+    }
+    return 0;
+}
+
+int lCompare2(NODE *node1, NODE *node2, char *field) {
+    if(node1 != NULL && node2 != NULL) {
+        if(strcmp(field,"firstName") == 0 ) {
+            return strcmp(node1->firstName, node2->firstName);
+
+        }
+
+        else if(strcmp(field,"lastName") == 0 ) {
+            return strcmp(node1->lastName, node2->lastName);
+        }
+        else if(strcmp(field,"email") == 0 ) {
+            return strcmp(node1->email, node2->email);
+        }
+        else if(strcmp(field,"birthDate") == 0 ) {
+            return strcmp(node1->birthDate, node2->birthDate);
+        }
+        else if(strcmp(field,"phone") == 0 ) {
+            return strcmp(node1->phone, node2->phone);
+        }
+        else {
+            fprintf(stderr, "Wrong argument!\n");
+            exit(EXIT_FAILURE);
+        }
+    }
+    return 0;
+}
+
+void lPrint(LIST *list) {
+    if(list != NULL) {
+        NODE *tmp = list->head;
+        while(tmp != NULL) {
+            lPrintPerson(tmp);
+            tmp = tmp->next;
+        }
+    }
+}
+
+void lPrintPerson(NODE *person) {
+    if(person != NULL) {
+        printf("Person: %s %s, %s, %s, %s\n",
+               person->firstName, person->lastName, person->birthDate,
+               person->email, person->phone);
+    }
+}
+//////////////////////////////////////////////////////////////////////
+
+TNODE *tcreateNode(char *firstName, char *lastName, char *birthDate,
+                 char *email, char *phone) {
+    TNODE *tmp = malloc(sizeof(TNODE));
+    if(tmp == NULL) {
+        fprintf(stderr, "Malloc failed!\n");
+        exit(EXIT_FAILURE);
+    }
+
+    memset(tmp->firstName, '\0', sizeof(tmp->firstName)); //allocate memory
+    strncpy(tmp->firstName, firstName, BUFFER_LEN); //save values
+
+    memset(tmp->lastName, '\0', sizeof(tmp->lastName));
+    strncpy(tmp->lastName, lastName, BUFFER_LEN);
+
+    memset(tmp->birthDate, '\0', sizeof(tmp->birthDate));
+    strncpy(tmp->birthDate, birthDate, BUFFER_LEN);
+
+    memset(tmp->email, '\0', sizeof(tmp->email));
+    strncpy(tmp->email, email, BUFFER_LEN);
+
+    memset(tmp->phone, '\0', sizeof(tmp->phone));
+    strncpy(tmp->phone, phone, BUFFER_LEN);
+
+    tmp->parent = NULL;
+    tmp->left = NULL;
+    tmp->right = NULL;
+
+    return tmp;
+}
+
+
+void setTreeKey(char *key, TREE *tree){
+    memset(tree->key, '\0', sizeof(tree->key));
+    strncpy(tree->key, key, BUFFER_LEN);
+
+}
+
+
+TREE *createTree() {
+    TREE *tmp = malloc(sizeof(TREE));
+    if(tmp == NULL) {
+        fprintf(stderr, "Malloc failed!\n");
+        exit(EXIT_FAILURE);
+    }
+    tmp->root = NULL;
+    setTreeKey("firstName", tmp); //default key
+    return tmp;
+}
+
+int tCompare(TNODE *node1, TNODE *node2) {
+    if(node1 != NULL && node2 != NULL) {
+        int cmpResult = strcmp(node1->firstName, node2->firstName);
+        if(cmpResult < 0) {
+            return -1;
+        } else if(cmpResult > 0) {
+            return 1;
+        } else {
+            return strcmp(node1->lastName, node2->lastName);
+        }
+    }
+    return 0;
+}
+
+int tCompare2(TNODE *node1, TNODE *node2, char *field) {
+    if(node1 != NULL && node2 != NULL) {
+        if(strcmp(field,"firstName") == 0 ) {
+            return strcmp(node1->firstName, node2->firstName);
+        }
+
+        else if(strcmp(field,"lastName") == 0 ) {
+            return strcmp(node1->lastName, node2->lastName);
+        }
+        else if(strcmp(field,"email") == 0 ) {
+            return strcmp(node1->email, node2->email);
+        }
+        else if(strcmp(field,"birthDate") == 0 ) {
+            return strcmp(node1->birthDate, node2->birthDate);
+        }
+        else if(strcmp(field,"phone") == 0 ) {
+            return strcmp(node1->phone, node2->phone);
+        }
+        else {
+            fprintf(stderr, "Wrong argument!\n");
+            exit(EXIT_FAILURE);
+        }
+    }
+    return 0;
+}
+
+void tPrintPerson(TNODE *person) {
+    if(person != NULL) {
+        printf("Person: %s %s, %s, %s, %s\n",
+               person->firstName, person->lastName, person->birthDate,
+               person->email, person->phone);
+    }
+}
+
+
+void tPrintInOrder(TREE *tree){
+    tPrintInOrderNode(tree->root);
+}
+
+void tPrintInOrderNode(TNODE *tnode){
+    if(tnode != NULL){
+        tPrintInOrderNode(tnode->left);
+        tPrintPerson(tnode);
+        tPrintInOrderNode(tnode->right);
+    }
+}
+
+void tAdd(TNODE *tnode, TREE *tree){
+    if(tnode != NULL && tree != NULL){
+        tree->root = tInsert(tnode, tree->root);
+    }
+}
+
+TNODE *tInsert(TNODE *tnode, TNODE *root){
+    if(tnode != NULL && root != NULL){
+        if(tCompare(tnode,root) < 0){
+            TNODE *curr = tInsert(tnode, root->left);
+            root->left = curr;
+            curr->parent = root;
+        }
+        else {
+            TNODE *curr = tInsert(tnode, root->right);
+            root->right = curr;
+            curr->parent = root;
+        }
+    }
+    else {
+        root = tnode;
+    }
+    return root;
+}
+
+TNODE *tInsertWithKey(TNODE *tnode, TNODE *root, char *key){
+    if(tnode != NULL && root != NULL){
+        if(tCompare2(tnode,root,key) < 0){
+            TNODE *curr = tInsertWithKey(tnode, root->left, key);
+            root->left = curr;
+            curr->parent = root;
+        }
+        else {
+            TNODE *curr = tInsertWithKey(tnode, root->right, key);
+            root->right = curr;
+            curr->parent = root;
+        }
+    }
+    else {
+        root = tnode;
+    }
+    return root;
+}
+
+void tAddWithKey(TNODE *tnode, TREE *tree, char *key){
+    if(tnode != NULL && tree != NULL){
+        tree->root = tInsertWithKey(tnode, tree->root, key);
+    }
+}
+
+void tDeletePom(TNODE *tnode){
+    if(tnode != NULL){
+        tDeletePom(tnode->left);
+        tDeletePom(tnode->right);
+        //printf("Deleting node: ");
+        //tPrintPerson(tnode);
+        free(tnode);
+    }
+}
+
+void tDeleteTree(TREE *tree){
+    tDeletePom(tree->root);
+    free(tree);
+}
+
+
+TNODE *tFindPerson( char *firstName, char *lastName, TREE *tree){
+    if(tree != NULL && tree->root != NULL){
+        return  tFindPersonPom(firstName, lastName, tree->root);
+    }
+    else return NULL;
+}
+
+TNODE *tFindPersonPom(char *firstName, char *lastName, TNODE *tnode){
+    if(tnode != NULL){
+        if( strcmp(tnode->firstName,firstName) == 0 && strcmp(tnode->lastName,lastName) == 0){
+            return tnode;
+        }
+        TNODE *leftResult = tFindPersonPom(firstName,lastName,tnode->left);
+        TNODE *rightResult = tFindPersonPom(firstName, lastName, tnode->right);
+        if(leftResult != NULL){
+            return leftResult;
+        }
+        if(rightResult != NULL){
+            return rightResult;
+        }
+    }
+    return NULL;
+}
+
+
+LIST *treeToListPom(TNODE *tnode, LIST *list){
+    if(tnode != NULL){
+
+
+        treeToListPom(tnode->left, list);
+        treeToListPom(tnode->right, list);
+        lAddFront(createNode(tnode->firstName, tnode->lastName, tnode->birthDate, tnode->email, tnode->phone),list);
+    }
+    return list;
+}
+
+LIST *treeToList(TREE *tree, LIST *list){
+    treeToListPom(tree->root, list);
+    return list;
+}
+
+
+TREE *tRebuild(TREE *tree, char *key){
+
+
+    LIST *list = createList();
+
+    TREE *tmp = createTree();
+    TNODE *tmp2 = NULL;
+    setTreeKey(key,tree);
+    list = treeToList(tree,list);
+    if(list != NULL) {
+        NODE *node = NULL;
+        if(list->head != NULL) {
+            node = list->head;
+            while(node != NULL) {
+                tmp2 = tcreateNode(node->firstName, node->lastName, node->birthDate, node->email, node->phone);
+                tAddWithKey(tmp2, tmp, key);
+                node = node->next;
+            }
+
+        }
+    }
+    TNODE *troot = tree->root;
+    tree->root = tmp->root;
+    lDeleteList(list);
+    tDeletePom(troot);
+    free(tmp);
+    return tree;
+}
+
+
+void tDeleteNode(TNODE *tnode, TREE *tree){
+
+    LIST *list = treeToList(tree, createList());
+    TREE *tmp = createTree();
+    lDeleteNode(lFindPerson(tnode->firstName,tnode->lastName, list), list);
+    char *key = tree->key;
+    TNODE *tmp2 = NULL;
+    if(list != NULL) {
+        NODE *node = NULL;
+        if(list->head != NULL) {
+            node = list->head;
+            while(node != NULL) {
+                tmp2 = tcreateNode(node->firstName, node->lastName, node->birthDate, node->email, node->phone);
+                tAddWithKey(tmp2, tmp, key);
+                node = node->next;
+            }
+
+        }
+    }
+    TNODE *troot = tree->root;
+    tree->root = tmp->root;
+    lDeleteList(list);
+    tDeletePom(troot);
+    free(tmp);
+}
+
---
+++ lab1/cw01/zad3/lib_dyn/mainDynamic.c
@@ -0,0 +1,256 @@
+#include "../../zad1/libaddrbook.h"
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <sys/resource.h>
+#include <stdint.h>
+#include <dlfcn.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/times.h>
+#include <unistd.h>
+#include <dlfcn.h>
+
+struct timeval uStart, uEnd, sStart, sEnd, rEnd, rStart;
+struct rusage ruStart, ruEnd;
+long double uSum, sSum, rSum;
+
+//dynamic library functions
+static LIST *(*_createList)();
+static void (*_lDeleteList)(LIST*);
+static NODE *(*_createNode)(char*, char*, char*, char*, char*);
+static void (*_lAddFront)(NODE*, LIST*);
+static NODE *(*_lFindPerson)(LIST*, char*, char*);
+static void (*_lPrintPerson)(NODE*);
+static NODE *(*_lFindByIndex)( int, LIST*);
+static void (*_lDeleteNode)(NODE*, LIST*);
+static void (*_lSort)(LIST*, char*);
+
+static TREE *(*_createTree)();
+static TNODE *(*_tcreateNode)(char*, char*, char*, char*, char*);
+static void (*_tAdd)(TNODE*, TREE*);
+static void (*_tDeleteNode)(TNODE*, TREE*);
+static TNODE *(*_tFindPerson)(char*, char*, TREE*);
+static void (*_tDeleteTree)(TREE*);
+static TREE *(*_tRebuild)(TREE*, char* );
+
+static void *handle;
+void *dlsymFunction(void *hanlde, const char* symbol);
+void loadDynamicLib();
+
+void startTime(){
+
+    getrusage(RUSAGE_SELF, &ruStart);
+    gettimeofday(&rStart, NULL);
+
+}
+void stopTime(){
+    getrusage(RUSAGE_SELF, &ruEnd);
+    gettimeofday(&rEnd, NULL);
+
+    uStart = ruStart.ru_utime;
+    uEnd = ruEnd.ru_utime;
+    sStart = ruStart.ru_stime;
+    sEnd = ruEnd.ru_stime;
+}
+
+void stopTimeAndPrint(){
+    stopTime();
+    printf("user: %09ld ms, system: %09ld ms, real: %09ld\n",
+           (( uEnd.tv_sec - uStart.tv_sec)*1000000L
+            + uEnd.tv_usec) - uStart.tv_usec,
+           (( sEnd.tv_sec - sStart.tv_sec)*1000000L
+            + sEnd.tv_usec) - sStart.tv_usec,
+           (( rEnd.tv_sec - rStart.tv_sec)*1000000L
+            + rEnd.tv_usec) - rStart.tv_usec);
+}
+
+int main(int argc, char *argv[])
+{
+    loadDynamicLib();
+    srand(time(NULL));
+    int iter = 1000;
+    char firstName[BUFFER_LEN];
+    char lastName[BUFFER_LEN];
+    char birthDate[BUFFER_LEN];
+    char email[BUFFER_LEN];
+    char phone[BUFFER_LEN];
+
+    printf("LIST:\n");
+    printf("Creating address book:\n");
+    startTime();
+    LIST *list = (*_createList)();
+    stopTimeAndPrint();
+
+
+
+    printf("Adding an element:\n");
+    uSum = 0; sSum = 0; rSum = 0;
+    for(int i = 0; i<iter; i++) //generate list with random names
+    {
+        startTime();
+        sprintf(firstName, "%d", i);
+        sprintf(lastName, "%d", rand());
+        sprintf(birthDate, "%d%d%d%d-%d-%d",1,9,rand()%10, rand()%10, rand()%12+1,rand()%31+1);
+        sprintf(email,"%d@com.pl",rand());
+        sprintf(phone,"%d%d%d-%d%d%d-%d%d%d", rand()%10, rand()%10, rand()%10, rand()%10,rand()%10, rand()%10, rand()%10,rand()%10,rand()%10);
+        (*_lAddFront)((*_createNode)(firstName,lastName,birthDate,email,phone),list);
+        stopTime();
+        uSum += (( uEnd.tv_sec - uStart.tv_sec)*1000000L + uEnd.tv_usec) - uStart.tv_usec;
+        sSum += (( sEnd.tv_sec - sStart.tv_sec)*1000000L + sEnd.tv_usec) - sStart.tv_usec;
+        rSum += (( rEnd.tv_sec - rStart.tv_sec)*1000000L + rEnd.tv_usec) - rStart.tv_usec;
+
+    }
+    printf("user: %09ld ms, system: %09ld ms, real: %09ld\n", (long int)uSum/iter, (long int)sSum/iter,(long int) rSum/iter);
+
+
+
+
+    printf("Deleting an element: optimistic: \n");
+    startTime();
+    (*_lDeleteNode)(list->head, list);
+    stopTimeAndPrint();
+
+
+    printf("Deleting an element: pesymistic: \n");
+    NODE *tmp = (*_createNode)("1001", "a", "a","a","a");
+    startTime();
+    (*_lDeleteNode)(tmp, list);
+    stopTimeAndPrint();
+
+
+    printf("Searching for an element: optimistic:\n");
+    startTime();
+    (*_lFindByIndex)(0,list);
+    stopTimeAndPrint();
+
+    printf("Searching for an element: pesymistic:\n");
+    startTime();
+    (*_lFindByIndex)(1001,list);
+    stopTimeAndPrint();
+
+    printf("Sort:\n");
+    startTime();
+    (*_lSort)(list,"lastName");
+    stopTimeAndPrint();
+
+//-----------tree-------
+    printf("TREE:\n");
+    printf("Create tree:\n");
+    startTime();
+    TREE *tree = (*_createTree)();
+    stopTimeAndPrint();
+
+    //(*_tcreateNode)(firstName,lastName,birthDate,email,phone);
+
+
+
+       printf("Adding an element:\n");
+       uSum = 0; sSum = 0; rSum = 0;
+
+       for(int i = 0; i<iter; i++){
+           startTime();
+           sprintf(firstName, "%d", i);
+           sprintf(lastName, "%d", 10 - i);
+           sprintf(birthDate, "%d%d%d%d-%d-%d",1,9,rand()%10, rand()%10, rand()%12+1,rand()%31+1);
+           sprintf(email,"%d@com.pl",rand());
+           sprintf(phone,"%d%d%d-%d%d%d-%d%d%d", rand()%10, rand()%10, rand()%10, rand()%10,rand()%10, rand()%10, rand()%10,rand()%10,rand()%10);
+           (*_tAdd)((*_tcreateNode)(firstName,lastName,birthDate,email,phone),tree);
+           stopTime();
+           uSum += (( uEnd.tv_sec - uStart.tv_sec)*1000000L + uEnd.tv_usec) - uStart.tv_usec;
+           sSum += (( sEnd.tv_sec - sStart.tv_sec)*1000000L + sEnd.tv_usec) - sStart.tv_usec;
+           rSum += (( rEnd.tv_sec - rStart.tv_sec)*1000000L + rEnd.tv_usec) - rStart.tv_usec;
+
+       }
+       printf("user: %09ld ms, system: %09ld ms, real: %09ld\n", (long int)uSum/iter, (long int)sSum/iter,(long int) rSum/iter);
+
+       printf("Deleting an element: optimistic: \n");
+       startTime();
+       (*_tDeleteNode)(tree->root, tree);
+
+       stopTimeAndPrint();
+
+
+       printf("Deleting an element: pesymistic: \n");
+       TNODE *tnode1 = tree->root;
+       while(tnode1-> left != NULL){
+           tnode1 = tnode1->left;
+       }
+
+       startTime();
+       (*_tDeleteNode)(tnode1, tree);
+       stopTimeAndPrint();
+
+
+       printf("Searching for an element: optimistic:\n");
+       startTime();
+       (*_tFindPerson)(tree->root->firstName, tree->root->lastName, tree);
+       stopTimeAndPrint();
+
+
+       printf("Searching for an element: pesymistic:\n");
+       TNODE *tnode = (*_tcreateNode)("1001", "a","a","a","a");
+       startTime();
+       (*_tFindPerson)(tnode->firstName, tnode->lastName, tree);
+       stopTimeAndPrint();
+
+       printf("Rebuild:\n");
+       startTime();
+    (*_tRebuild)(tree, "lastName");
+       stopTimeAndPrint();
+
+
+
+
+    (*_tDeleteTree)(tree);
+    (*_lDeleteList)(list);
+    return 0;
+}
+
+
+void loadDynamicLib() {
+    //opening and loading dynamic library
+
+    handle = dlopen("libdynamic.so", RTLD_NOW);
+    if(handle == NULL) {
+        fprintf(stderr, "%s\n", dlerror());
+        exit(EXIT_FAILURE);
+    }
+
+    dlerror();
+
+    //loading functions
+    _createList = dlsymFunction(handle, "createList");
+    _lDeleteList = dlsymFunction(handle, "lDeleteList");
+    _createNode = dlsymFunction(handle, "createNode");
+    _lAddFront = dlsymFunction(handle, "lAddFront");
+    _lFindPerson = dlsymFunction(handle, "lFindPerson");
+    _lPrintPerson = dlsymFunction(handle, "lPrintPerson");
+    _lFindByIndex = dlsymFunction(handle, "lFindByIndex");
+    _lDeleteNode = dlsymFunction(handle, "lDeleteNode");
+    _lSort = dlsymFunction(handle, "lSort");
+
+    _createTree = dlsymFunction(handle, "createTree");
+    _tcreateNode = dlsymFunction(handle, "tcreateNode");
+    _tAdd = dlsymFunction(handle, "tAdd");
+    _tDeleteNode = dlsymFunction(handle, "tDeleteNode");
+    _tFindPerson = dlsymFunction(handle, "tFindPerson");
+    _tRebuild = dlsymFunction(handle, "tRebuild");
+    _tDeleteTree = dlsymFunction(handle, "tDeleteTree");
+}
+
+void *dlsymFunction(void *hanlde, const char* symbol) {
+    void *tmp = dlsym(handle, symbol);
+    char *error;
+
+    if((error = dlerror()) != NULL) {
+        fprintf(stderr, "%s\n", dlerror());
+        exit(EXIT_FAILURE);
+    }
+    return tmp;
+}
+
+
