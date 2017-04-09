0f3bcd88867d83dcde18c31b1c0d146a713d2e35
oswinoswin <oswinoswin@users.noreply.github.com>
2017-04-09 20:25:44 +0200

    should add memory managment

--- cw05/zad1/interpreter.c
+++ cw05/zad1/interpreter.c
@@ -1,232 +1,77 @@
 #include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <sys/wait.h>
-#include <memory.h>
 
-#define BUFFER_LEN	256
-
-typedef struct COMMAND{
-    struct COMMAND *next;
-    char arguments[BUFFER_LEN][BUFFER_LEN];
-} COMMAND;
-
-char **split_str(char *str);
-
-char **split_str_by_pipe(char *str, int *num);
-
-
-int main(int argc, char **argv)
+struct command
 {
-
-    int pipefd[2];
-    int pid;
-    char **to_execute;
-    char **args1;
-    char **args2;
-    char ** args3;
-    int count;
-    int status;
-
-    COMMAND command;
-    char start[] = "ls -l | head -6 | tail -5 | tail -1\0";
-    to_execute = split_str_by_pipe(start, &count);
-    //printf("count = %d\n", count);
-
-    char **argsy[count];
-    for(int i = 0; i<count; i++){
-        argsy[i] = split_str(to_execute[i]);
-        //printf("argsy[%d] %s\n", i, argsy[i][0]);
-    }
+    const char **argv;
+};
 
 
-/*
-    char *str1 = to_execute[0];
-    char *str2 = to_execute[1];*/
+int spawn_proc (int in, int out, struct command *cmd)
+{
+    pid_t pid;
 
-
-   /* args1 = split_str(str1);
-    args2 = split_str(str2);
-    args3 = split_str(to_execute[2]);*/
-
-
-
-    int i = 0;
-    int numPipes = count - 1;
-
-
-
-    int pipefds[2*numPipes];
-
-    for(i = 0; i < 2*(numPipes); i++){
-        if(pipe(pipefds + i*2) < 0) {
-            perror("pipe");
-            exit(EXIT_FAILURE);
+    if ((pid = fork ()) == 0)
+    {
+        if (in != 0)
+        {
+            dup2 (in, 0);
+            close (in);
         }
-    }
 
-    for(int j = 0; j<count; j++){
-        //printf("j = %d\n",j);
-        //printf("Should execute %s\n", argsy[j][0]);
-        pid = fork();
-        if (pid == 0) {
-            sleep(1);
-
-            //if not first command
-            if (j != 0) {
-                //printf("j!=0\n");
-               if (dup2(pipefds[(j - 1) * 2], 0) < 0) {
-                    perror(" dup2");///j-2 0 j+1 1
-                    exit(EXIT_FAILURE);
-
-                }
-                //printf("j = %d  dup(pipefd[%d], 0])\n",j, (j-1)*2);
-            }
-            //if not last command
-            //printf("j = %d, count-1 =%d\n", j, count-1);
-
-            if(j < count - 1) {
-                printf("j<count -1\n");
-                if(dup2(pipefds[j * 2 + 1], 1) < 0) {
-                    perror("dup2");
-                    exit(EXIT_FAILURE);
-                }
-                printf("j = %d dup(pipefd[%d], 1])\n", j,2*j+1);
-            } else{
-                printf("Now we should get result\n");
-            }
-
-            for (i = 0; i < 2 * numPipes; i++) {
-                close(pipefds[i]);
-            }
-
-           // printf("j = %d %s\n",j, argsy[j][0]);
-            fprintf(stderr, "j = %d %s\n",j, argsy[j][0]);
-            if (execvp(argsy[j][0], argsy[j]) < 0) {
-                perror(argsy[j][0]);
-                exit(EXIT_FAILURE);
-            }
-
-
+        if (out != 1)
+        {
+            dup2 (out, 1);
+            close (out);
         }
-        else if(pid < 0) {
-                perror("error");
-                exit(EXIT_FAILURE);
-            }
 
-
+        return execvp (cmd->argv [0], (char * const *)cmd->argv);
     }
-    for(i = 0; i < 2 * numPipes; i++){
-       close(pipefds[i]);
-        //puts("closed pipe in parent");
-    }
-    while(waitpid(0,0,0) <= 0);
 
-
-
-    return 0;
+    return pid;
 }
 
+int fork_pipes (int n, struct command *cmd)
+{
+    int i;
+    pid_t pid;
+    int in, fd [2];
 
-char **split_str(char *str) {
-    char * pch;
+    /* The first process should get its input from the original file descriptor 0.  */
+    in = 0;
 
-    int count = 0;
-    char *last_comma;
-    char **result;
-
-    while(strncmp(str, " ", 1) == 0) { //get rid of spaces int the begining
-        str++;
-    }
-
-    char *tmp = str;
-
-
-    while (*tmp)
+    /* Note the loop bound, we spawn here all, but the last stage of the pipeline.  */
+    for (i = 0; i < n - 1; ++i)
     {
-        if (strncmp(tmp, " ", 1) == 0)
-        {
-            count++;
-            last_comma = tmp;
-        }
-        tmp++;
+        pipe (fd);
+
+        /* f [1] is the write end of the pipe, we carry `in` from the prev iteration.  */
+        spawn_proc (in, fd [1], cmd + i);
+
+        /* No need for the write end of the pipe, the child will write here.  */
+        close (fd [1]);
+
+        /* Keep the read end of the pipe, the next child will read from there.  */
+        in = fd [0];
     }
 
-    count += last_comma < (str + strlen(str) - 1); //increase count if the last el wasn't delim
+    /* Last stage of the pipeline - set stdin be the read end of the previous pipe
+       and output to the original file descriptor 1. */
+    if (in != 0)
+        dup2 (in, 0);
 
-    result = (char **)malloc((count+1)*sizeof(char*));
-    if(result == NULL){
-        fprintf(stderr,"malloc error\n");
-        EXIT_FAILURE;
-    }
-
-    int i = 0;
-    pch = strtok (str," ,");
-    if(pch == NULL){
-        fprintf(stderr, "split string error\n");
-        EXIT_FAILURE;
-    }
-    else{
-        result[i] = pch;
-        i++;
-    }
-
-
-    do{
-
-        pch = strtok (NULL, " ,\n");
-        result[i] = malloc(sizeof(char)*strlen(pch));
-
-        strcpy(result[i], pch);
-        i++;
-    }while (pch != NULL && i<count);
-
-    result[count] = NULL;
-    return result;
+    /* Execute the last stage with the current process. */
+    return execvp (cmd [i].argv [0], (char * const *)cmd [i].argv);
 }
 
-char **split_str_by_pipe(char *str, int *num) {
-    char * pch;
-    char *tmp = str;
-    int count = 0;
-    char *last_comma;
-    char **result;
+int
+main ()
+{
+    const char *ls[] = { "ls", 0 };
+    const char *awk[] = { "awk", "{print $1}", 0 };
+    const char *sort[] = { "sort", 0 };
+    const char *uniq[] = { "uniq", 0 };
 
-    while (*tmp)
-    {
-        if (strncmp(tmp, "|", 1) == 0)
-        {
-            count++;
-            last_comma = tmp;
-        }
-        tmp++;
-    }
-    count += last_comma < (str + strlen(str) - 1); //increase count if the last el wasn't delim
-    result = (char **)malloc((count+1)*sizeof(char*));
-    if(result == NULL){
-        fprintf(stderr,"malloc error\n");
-        EXIT_FAILURE;
-    }
+    struct command cmd [] = { {ls}, {awk}, {sort}, {uniq} };
 
-    int i = 0;
-    pch = strtok (str,"|");
-    if(pch == NULL){
-        fprintf(stderr, "split string error\n");
-        EXIT_FAILURE;
-    }
-    else{
-        result[i] = pch;
-        i++;
-    }
-    while (pch != NULL && i<count)
-    {
-        pch = strtok (NULL, "|\n");
-        result[i] = malloc(sizeof(char)*strlen(pch));
-        strcpy(result[i], pch);
-        i++;
-    }
-    result[count] = NULL;
-
-    *num = count;
-    return result;
+    return fork_pipes (1, cmd);
 }
